e0173deec225c23f309afadfffb734afc28aec49
FearGod12 <onyenikechukwudi@gmail.com>
2023-08-03 23:15:17 +0100

    task 1 docs

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,7 +3,7 @@
 """
 
 import re
-from typing import List
+from typing import List, Iterable
 import logging
 
 
@@ -24,10 +24,12 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields):
+    def __init__(self, fields: List[str]):
+        """the class constructor"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """formats the record overriding the default formatter"""
         return filter_datum(self.fields, self.REDACTION,
                             super().format(record), self.SEPARATOR)
