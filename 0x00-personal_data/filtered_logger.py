0c20c7b871fed1dc8bd3826323a42bc6bc7d8c74
FearGod12 <onyenikechukwudi@gmail.com>
2023-08-03 22:29:24 +0100

    task 0

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""the filtered logger module
+"""
+
+import re
+from typing import List
+import logging
+
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """ Replacing """
+    for entry in fields:
+        message = re.sub(rf"{entry}=(.*?)\{separator}",
+                         f'{entry}={redaction}{separator}', message)
+    return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
