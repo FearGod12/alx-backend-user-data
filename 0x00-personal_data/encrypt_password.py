1001fe4f3623f5f9cac81702770a1f34b4ba28c0
FearGod12 <onyenikechukwudi@gmail.com>
2023-08-04 17:17:05 +0100

    task 6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -12,4 +12,4 @@ def hash_password(password: str) -> bytes:
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """checks if hashed password and the string password matches"""
-    return bcrypt.checkpw(password, hashed_password)
+    return bcrypt.checkpw(password.encode("utf-8"), hashed_password)
