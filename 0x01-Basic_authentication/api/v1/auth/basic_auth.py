ccafc3f3f56c553179b2b92c388a08285f00d123
FearGod12 <onyenikechukwudi@gmail.com>
2023-08-08 14:25:18 +0100

    task 6

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+basic authentication module
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """the class to be used for basic authentication"""
+    pass
