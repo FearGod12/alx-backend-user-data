bd42c82f1810798a7252f9dc013a07aebe10653b
FearGod12 <onyenikechukwudi@gmail.com>
2023-08-08 15:29:46 +0100

    task 7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,8 @@
 """
 basic authentication module
 """
+import base64
+import binascii
 
 from api.v1.auth.auth import Auth
 
@@ -12,9 +14,23 @@ class BasicAuth(Auth):
     def extract_base64_authorization_header(self,
                                             authorization_header: str) -> str:
         """extracts the base64 authorization header"""
-        if authorization_header is None or type(authorization_header)\
+        if authorization_header is None or type(authorization_header) \
                 is not str:
             return None
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str) \
+            -> str:
+        """decodes the base64 authorization header"""
+        if base64_authorization_header is None \
+                or type(base64_authorization_header) is not str:
+            return None
+        try:
+            credential = base64.b64decode(base64_authorization_header
+                                          ).decode("utf-8")
+            return credential
+        except binascii.Error:
+            None
