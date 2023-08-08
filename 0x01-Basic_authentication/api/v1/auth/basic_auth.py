0f10506835366afe1c9fdb52da31eb488b10ce30
FearGod12 <onyenikechukwudi@gmail.com>
2023-08-08 15:38:55 +0100

    task 8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -34,3 +34,15 @@ class BasicAuth(Auth):
             return credential
         except binascii.Error:
             None
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header: str)\
+            -> (str, str):
+        """extracts user credentials from the base64 encoded data"""
+        if not decoded_base64_authorization_header \
+                or type(decoded_base64_authorization_header) is not str:
+            return None, None
+        if ":" not in decoded_base64_authorization_header:
+            return None, None
+        email, passwd = decoded_base64_authorization_header.split(":")
+        return email, passwd
