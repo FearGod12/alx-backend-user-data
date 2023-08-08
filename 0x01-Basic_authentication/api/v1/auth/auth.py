ed40621247dcddd7b164f8bd1f181ff6caefbc9b
FearGod12 <onyenikechukwudi@gmail.com>
2023-08-08 12:29:39 +0100

    task 4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -15,7 +15,11 @@ class Auth:
         :param excluded_paths:
         :return: False
         """
-        return False
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path in excluded_paths or ((path + "/") in excluded_paths):
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
